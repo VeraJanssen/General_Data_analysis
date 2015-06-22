099164e8649aeffa6724baa68ccd69d12e610ca4
unknown <verajanssen@TUD206905.tudelft.net>
2015-06-22 15:31:30 +0200

    resistance plot in working function

--- mobility.py
+++ mobility.py
@@ -34,7 +34,7 @@ Vsd = (0.05, 0.025, 0,-0.025,-0.05)
 IVsd = np.empty(297)
 const_gate = 0
 mlin = np.empty(1)
-Rarr = 0
+
 
 
 """""""""""""Functions"""""""""""""
@@ -48,8 +48,7 @@ def make_array_IVs(source_dir, IVsd, const_gate):
             const_gate = np.column_stack([const_gate, file])
     return IVsd, const_gate            
     
-def plot_ivsd(bias, IVsd, file):
-       
+def plot_ivsd(bias, IVsd, file):       
     rawIVsd = plt.figure()    
     for i in range(np.size(IVsd,1)-1):
         plt.plot(bias*1e3, IVsd[:,i+1]*1e6, label=file)
@@ -60,6 +59,14 @@ def plot_ivsd(bias, IVsd, file):
     plt.show(rawIVsd)
     
 
+def plot_R(bias, IVsd):   
+    Rarr = np.empty(1)
+    for i in range(np.size(IVsd,1)-1):
+        R = 1/np.polyfit(bias,IVsd[:,i+1],1)[0]
+        Rarr = np.column_stack([Rarr, R])
+    plt.figure()
+    plt.scatter(np.linspace(-1.1,-2,10), Rarr[0,1:11])
+
 "Start script"
 
  # open IVsd files from IVsd folder
@@ -68,12 +75,12 @@ source_dir = 'C:/Users/verajanssen/SURFdrive/Werk/Science/Projects/[QDs]Hallbar_
 IVsd, const_gate = make_array_IVs(source_dir, IVsd, const_gate)
 
 
-bias = IVsdnew[:,0] #in V   
-
+#bias = IVsdnew[:,0] #in V   
 plot_ivsd(bias, IVsd, file)
 
-R = 1/np.polyfit(bias,IVsd[:,i+1],1)[0]
-Rarr = np.column_stack([Rarr, R])
+
+plot_R(bias, IVsd)
+
         
         
 
@@ -85,6 +92,7 @@ lin_region_start = 620
 lin_region_end = 680
 
 #rawIVg = plt.figure()
+plt.figure()
 for i in range(np.size(IVg, 0)):
     #plt.plot(gate[lin_region_start:lin_region_end], IVg[i, lin_region_start:lin_region_end])
     #subtract the Vg = 0V (background) curve. Use the middle column. Will give error when the number of measurements is odd (than there is no Vg = 0V) 
@@ -97,7 +105,7 @@ for i in range(np.size(IVg, 0)):
     mlin = np.column_stack([mlin, mlinnew[0]]) 
     sigma = -(length*normIVg)/(width*0.25*Vsd[i])
     mobility = sigma/(e*n)
-    
+
     if i==0:
         plt.plot(gate,  normIVg*1e9, label = 'Vsd = -50mV' )
     if i==1:
@@ -115,8 +123,7 @@ for i in range(np.size(IVg, 0)):
 
 #Plot resistance from IVsd's
 
-plt.figure()
-#plt.scatter(np.linspace(-1.1,-2,10), Rarr[0,1:11])
+
 
 
  # check for loop or seperate files
